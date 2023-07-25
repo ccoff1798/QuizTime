5bb0e3a74a3beba1fca6aa0593747c78dae3d019
Cody <ccoff1798@gmail.com>
2023-07-25 13:15:07 -0600

    got css started, logic is working, highscore system instead of old not working scoregetter function

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 //High score button
 let start = document.querySelector('#startGame');
 let body = document.querySelector('#body');
-let section = document.createElement("section");
+let highscoreButton = document.querySelector('#highscore')
+let sectionEl = document.createElement("section");
+let articleEl = document.createElement('article')
 let quizPlace = 0
 let answersCorrect = 0
+let highscore = 0
 // let questionSection = document.body.appendChild(section);
 
 
@@ -15,6 +18,8 @@ start.addEventListener("click", function () {
     console.log("clicked");
     init();
 })
+
+highscoreButton.addEventListener("click", getHighscore)
 //Set up the initialize function onlick
 function init() {
     generateQuestions()
@@ -22,12 +27,12 @@ function init() {
 }
 //Second I need to get the questions set up
 let question1 = {
-    question: 'q1',
-    choice1: 'a',
-    choice2: 'b',
-    choice3: 'c',
-    choice4: 'D',
-    answer: 'b'
+    question: "Through the Series, the Bender is comprised of all of the folowing except What? ",
+    choice1: '40% Iron',
+    choice2: '40% Titanium',
+    choice3: '40% Dolomite',
+    choice4: '40% Diorite',
+    answer: '40% Diorite'
 }
 let question2 = {
     question: 'q2',
@@ -58,8 +63,12 @@ function generateQuestions() {
     console.log(currentQuestion)
     //set up the section that hold the questions
     let questionEl = document.createElement("div");
+    questionEl.classList.add("quizContanier")
     questionEl.classList.add("quiz")
     questionEl.textContent = currentQuestion.question;
+
+    let articleEl = document.createElement('article')
+    articleEl.classList.add("buttonContainer")
 
     let answer1El = document.createElement("button");
     answer1El.classList.add("quiz")
@@ -81,12 +90,13 @@ function generateQuestions() {
     answer4El.textContent = currentQuestion.choice4;
     answer4El.addEventListener('click', checkAnswers)
 
-    section.appendChild(questionEl)
-    section.appendChild(answer1El)
-    section.appendChild(answer2El)
-    section.appendChild(answer3El)
-    section.appendChild(answer4El)
-    let questionSection = document.body.appendChild(section);
+    sectionEl.appendChild(questionEl)
+    sectionEl.appendChild(articleEl)
+    articleEl.appendChild(answer1El)
+    articleEl.appendChild(answer2El)
+    articleEl.appendChild(answer3El)
+    articleEl.appendChild(answer4El)
+    let questionSection = document.body.appendChild(sectionEl);
 
 }
 //I need to set up some way to swap out the questions with math.rando,
@@ -104,16 +114,38 @@ function checkAnswers(event) {
     quizPlace++
     // let clearlog = document.getElementsByClassName('quiz')
     document.querySelectorAll('.quiz').forEach(e => e.remove());
-    if(quizPlace < questionArray.length){
-    generateQuestions()
-    } else{
+    if (quizPlace < questionArray.length) {
+        generateQuestions()
+    } else {
         alert("Quiz Over")
-        localStorage.setItem("Score",answersCorrect)
+        if (answersCorrect > localStorage.getItem("Highscore")) {
+            localStorage.setItem("Highscore", answersCorrect);
+        }
     }
-} 
+}
 
-function scorekeeper(){
-    localStorage.getItem("score")
+// function scorekeeper() {
+//     let scorelist = localStorage.getItems("score")
+//     console.log(scorelist)
+// }
+
+    
+
+
+
+function getHighscore() {
+    // Get the highscore from localStorage
+    let highscore = localStorage.getItem("Highscore");
+    if(highscore === null) {
+        highscore = 0;
+    }
+
+    // Create a new div to display the highscore
+    let highscoreDiv = document.createElement("div");
+    highscoreDiv.textContent = "Highscore: " + highscore;
+
+    // Add the highscore div to the body of the document
+    document.body.appendChild(highscoreDiv);
 }
 
 
